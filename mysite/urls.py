f8a3cba87729348b480970ae6b303599b7ed041c
Bill <realoption11@gmail.com>
2014-12-24 08:37:33 -0500

    l

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from mysite.views import Index
+from app1 import views
 
 admin.autodiscover()
 
@@ -9,7 +10,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', Index.as_view(), name='index'),
+    url(r'^index/', 'app1.views.index', name='index'),
+
+    # url(r'^$', Index.as_view(), name='index'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
